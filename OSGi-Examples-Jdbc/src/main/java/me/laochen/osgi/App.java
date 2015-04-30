b3a2d8ed1e1c9d9bad876943502e2f1eec22dfd6
simple <3gwind@gmail.com>
2015-04-30 14:36:52 +0800

    init sub project

---
+++ OSGi-Examples-Jdbc/src/main/java/me/laochen/osgi/App.java
@@ -0,0 +1,13 @@
+package me.laochen.osgi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
