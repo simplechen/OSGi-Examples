ee3acfb29fcee6eb0d7ba81ad9c69243bc8fc0de
simple <3gwind@gmail.com>
2015-05-04 11:50:16 +0800

    配置ant插件,可以直接通过eclipse编译并安装

--- OSGi-Examples-Camel/src/main/java/me/laochen/osgi/handler/SimpleHandler.java
+++ OSGi-Examples-Camel/src/main/java/me/laochen/osgi/handler/SimpleHandler.java
@@ -14,9 +14,9 @@ public class SimpleHandler implements Processor {
 	@Override
 	public void process(Exchange exchange) throws Exception {
 		HttpServletRequest request = exchange.getIn().getBody(HttpServletRequest.class);
-		exchange.getOut().setHeader("Content-Type", "text/plain;charset=utf-8");
+		exchange.getOut().setHeader("Content-Type", "text/html;charset=utf-8");
 		
-		String reponseBody = "-- Hello Jetty! --(notice:you can use param. for instance: http://<ip>:<port>/hello?uid=2&uname=simple";		
+		String reponseBody = "[Camel Base Servicemix-5.4.0 and jetty] <br /> Hello Jetty! <br />(<font color=red>notice:you can use param. for instance: http://<ip>:<port>/hello?uid=2&uname=simple</font>";		
 		Map<String,String[]> params = request.getParameterMap();
 		if(params.size()>0){
 			StringBuffer sbuffer = new StringBuffer();
